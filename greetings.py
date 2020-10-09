c32ca37b09fabaf5122f3ac60cbc87a58988c870
Stephany Pena <faithfulwater@gmail.com>
2020-10-08 21:32:00 -0400

    added and finished rest of the lab.

---
+++ greetings.py
@@ -0,0 +1,5 @@
+#create a variable to store name of user
+#2. Input is a function
+username = input("What's your name? ")
+#concatenate strings, print result to the terminal
+print("Hi " + str(username) + "!")
---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#Making variables noun, adjective, adverb, verb
+noun = input("Please input a noun. ")
+adjective = input("Please input an adjective. ")
+adverb = input("Please input an adverb. ")
+verb = input("Please input a verb. ")
+
+#concatenate strings, print result to the terminal
+print("On a tropical island " + str(noun) + ",")
+print("Underneath a " + str(adjective) + " lava moon.")
+print("Hangin' " + str(adverb) + " with the hula dancers,")
+print(str(verb) + " questions cause" + " they got all the answers.")
